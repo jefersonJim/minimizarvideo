a216fd711b5afe7ff64fd64668c94f753fd95091
Jeferson Inacio Macedo <jeferson.inacio@seduc.ce.gov.br>
2019-05-07 10:05:40 -0300

    fix error udemy's site

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function getOffset(el) {
   const rect = el.getBoundingClientRect();
   return {
@@ -6,74 +5,64 @@ function getOffset(el) {
     top: rect.top + window.scrollY
   };
 }
-var imgURL = chrome.runtime.getURL("minimize.png");
+
+$(document).ready(function(){
+	addBtn();	
+	function addBtn(){
+		let isYoutube = (document.location.href.indexOf('youtube.com') !== -1)
+		var imgURL = chrome.runtime.getURL("minimize.png");
+		document.querySelectorAll('video').forEach(function(video,index){
+			var video_top = getOffset(video).top;
+			var video_left = getOffset(video).left;
+
+			var  minus  = 60;
+
+			var ajust = (video.offsetWidth/2)-minus;
 
 
-var times = 0;
-
-var interval = setInterval(function(){
-  times += 1
-  if(document.querySelectorAll('video').length != 0){
-  	clearInterval(interval);
-  	setTimeout( addBtn(), 1000);
-  }else if(times == 100){
-  	clearInterval(interval);
-  }
-},100)
-
-function addBtn(){
-	let isYoutube = (document.location.href.indexOf('youtube.com') !== -1)
-	document.querySelectorAll('video').forEach(function(video,index){
-		var video_top = getOffset(video).top;
-		var video_left = getOffset(video).left;
-
-		var  minus  = 60;
-
-		var ajust = (video.offsetWidth/2)-minus;
-
-
-		var container = $(`
-			<div id="_v_`+index+`" class="container">
-				<div class="button-container" style="z-index:999999999999999;">
-					<div class="custom-button-wrapper">
-						<div class="custom-button" id="video-detach-button" aria-label="Detach button">
-							<div class="custom-button-contents custom-button-image">
-								<div class="image" data-video="`+index+`">
-									<img style="width: 14px; margin-top:-15px;" src="`+imgURL+`"/>
+			var container = $(`
+				<div id="_v_`+index+`" class="container">
+					<div class="button-container" style="z-index:999999999999999;">
+						<div class="custom-button-wrapper">
+							<div class="custom-button" id="video-detach-button" aria-label="Detach button">
+								<div class="custom-button-contents custom-button-image">
+									<div class="image" data-video="`+index+`">
+										<img style="width: 14px; margin-top:-15px;" src="`+imgURL+`"/>
+									</div>
 								</div>
+								<div class="custom-button-contents custom-button-text"></div>
 							</div>
-							<div class="custom-button-contents custom-button-text"></div>
 						</div>
 					</div>
 				</div>
-			</div>
-		`);
+			`);
 
-		$("body").append(container);
-		$("#_v_"+index).css({"top":video_top+"px", "left":video_left+ajust+"px"});
+			$("body").append(container);
+			$("#_v_"+index).css({"top":video_top+"px", "left":video_left+ajust+"px"});
 
-		$( video ).hover(function() {
+			$( video ).hover(function() {
+					let ajustTop = (isYoutube ? 5 : 50);
+					var video_top = getOffset(video).top+ajustTop;
+					var video_left = getOffset(video).left;
+					var ajust = (video.offsetWidth/2)-minus;
+					$("#_v_"+index).css({"top":video_top+"px", "left":video_left+ajust+"px"});
+					$("#_v_"+index+" .button-container").addClass("visible");
+			},function() {
+					$("#_v_"+index+" .button-container").removeClass("visible");
+			});
+
+			$("#_v_"+index+" .button-container").hover(function() {
 				let ajustTop = (isYoutube ? 5 : 50);
-		    var video_top = getOffset(video).top+ajustTop;
-		    var video_left = getOffset(video).left;
-			  var ajust = (video.offsetWidth/2)-minus;
-			  $("#_v_"+index).css({"top":video_top+"px", "left":video_left+ajust+"px"});
-		    $("#_v_"+index+" .button-container").addClass("visible");
-		},function() {
-		    $("#_v_"+index+" .button-container").removeClass("visible");
-		});
+				var video_top = getOffset(video).top+ajustTop;
+				var video_left = getOffset(video).left;
+				var ajust = (video.offsetWidth/2)-minus;
+				$("#_v_"+index).css({"top":video_top+"px", "left":video_left+ajust+"px"});
+				$("#_v_"+index+" .button-container").addClass("visible");
+			});
 
-		$("#_v_"+index+" .button-container").hover(function() {
-			let ajustTop = (isYoutube ? 5 : 50);
-		  var video_top = getOffset(video).top+ajustTop;
-		  var video_left = getOffset(video).left;
-		  var ajust = (video.offsetWidth/2)-minus;
-		  $("#_v_"+index).css({"top":video_top+"px", "left":video_left+ajust+"px"});
-		  $("#_v_"+index+" .button-container").addClass("visible");
+			$("#_v_"+index+" .button-container").click(function(){
+				video.requestPictureInPicture();
+			})
 		});
-
-		$("#_v_"+index+" .button-container").click(function(){
-			video.requestPictureInPicture();
-		})
-	});
-}
+	}
+});
